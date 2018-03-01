f428574aa9d3b07334a4c0376b35bc2be11599d4
YHF <773768513@qq.com>
2018-03-01 13:30:49 +0800

    add test module

---
+++ UseCommandFindSpecialLine/test/StorageModule.h
@@ -0,0 +1,22 @@
+/********************************************//**
+             Storage input string  module
+*-
+@file    StorageInputStr.h
+@author  YHF
+@data  2018/2/27
+@brief  Input character and storage to memeory
+******************************************************/
+#ifndef __STORAGE_MODULE__
+#define __STORAGE_MODULE__
+///
+///define the fixed length 
+#ifndef __READ_SIZE__
+#define __READ_SIZE__
+#define READ_SIZE 128
+#endif
+
+int JudgeMemory(char* pJudgeMemory);
+char MallocAndCopy(char** pMallocMemory,char *pOriginData,int CopyLength);
+FILE* ReadCompleteLine(char *pGetLine,int* pMemoryLength,FILE* fpReadFile);
+
+#endif
---
+++ UseCommandFindSpecialLine/test/TestModule.cpp
@@ -0,0 +1,19 @@
+
+/****************************//**
+          test module
+@file  CommandLineInput.cpp
+@author YHF
+@data  2018/2/25
+@brief
+*****************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "MatchStrlenFile.h"
+#include "StorageModule.h"
+#include "CommandLineInputMain.h"
+
+
+
+
