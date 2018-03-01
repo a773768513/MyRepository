51be6c351d7cd9ef6d36e3cb8c4699d5bf7942a6
YHF <773768513@qq.com>
2018-03-01 16:46:26 +0800

    new

---
+++ FindSpecialLineUseCommand/FindSpecialLineUseCommand/CommandLineInputMain.cpp
@@ -0,0 +1,63 @@
+/****************************//**
+          CommandLine input module
+@file  CommandLineInput.cpp
+@author YHF
+@data  2018/2/25
+@brief
+*****************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "MatchStrlenFile.h"
+#include "StorageModule.h"
+#include "CommandLineInputMain.h"
+
+/****************************************************//**
+@brief  main
+@param  argc    the param number of  command line
+@param  argv[]   
+@retval 1  error input param
+@author  YHF
+@data   2018/2/25
+@
+********************************************************/
+int main(int argc,char* argv[])
+{
+	char* pInputStr;              
+	int   InputStrLength;
+	char* pInputFile;
+	char* pOutputFile;
+
+	if(argc !=5)
+	{
+		if (!strcmp(argv[1],"-help"))
+		{
+			HelpCommandPrintf();
+			return HELP;
+		}
+		printf("input error,you must input data inthe format:\nprogram  string  OriginFilePath   OutputFileDataPath");
+		return ARGUMENT_COUNT_ERROR;
+	}
+	pInputStr = argv[1];
+	InputStrLength = strlen(pInputStr);
+	pInputFile = argv[2];
+	pOutputFile = argv[3];
+	char* pStandardFile = argv[4];
+	MatchStrInFile(pInputStr,InputStrLength,pInputFile,pOutputFile,pStandardFile);
+	return SUCCESS;
+
+}
+/*****************************************************//*
+@brief   -help  give user message to operation
+@param   NO only output message
+@author YHF
+@data  2018/2/25
+@brief
+
+*******************************************************/
+void HelpCommandPrintf()
+{
+	printf("use the follow format to input the data\n program  string  OriginFilePath  OutputFileDataPath \n");
+	printf("prompt:the Path must be less than 260 character, \n");
+	printf("-help  check this tips \n");
+}
---
+++ FindSpecialLineUseCommand/FindSpecialLineUseCommand/CommandLineInputMain.h
@@ -0,0 +1,15 @@
+#ifndef __COMMAND_LINE_INPUT_MAIN__
+#define __COMMAND_LINE_INPUT_MAIN__
+
+#ifndef __RETURN_VALUE__
+#define __RETURN_VALUE__
+#define SUCCESS                  0
+#define ARGUMENT_COUNT_ERROR     1
+#define	HELP                     3
+
+#endif
+
+
+void HelpCommandPrintf();
+
+#endif
